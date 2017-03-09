e2d9900b80f4d11e5af3a61d83ee9d0aff2500b9
rihel <569843207@qq.com>
2017-03-10 01:01:30 +0800

    修改

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,34 +1,29 @@
-$(function(){
+$('.wp-inner').fullpage({
+	page: '.page',
+	change: function (e) {
 
-	$('.wp-inner').fullpage({
-		page: '.page',
-		change: function (e) {
+	},
+	beforeChange: function (e) {
+		$('.page').eq(e.cur).find('.animated').removeClass('active');
+	},
+	afterChange: function (e) {
+		$('.page').eq(e.cur).find('.animated').addClass('active');
+		if(Number(e.cur)===3){
+			$('.gonextpage').addClass('active');
+			$('.gonextpage').data('dir','movePrev');
+		}else {
+			$('.gonextpage').removeClass('active');
+			$('.gonextpage').data('dir','moveNext');
+		}
+	},
+	orientationchange: function () {}
+});
+$('.page').on('change',function(){
 
-		},
-		beforeChange: function (e) {
-			$('.page').eq(e.cur).find('.items').removeClass('active');
-		},
-		afterChange: function (e) {
-			$('.page').eq(e.cur).find('.items').addClass('active');
-			if(Number(e.cur)===3){
-				$('.gonextpage').addClass('active');
-				$('.gonextpage').data('dir','movePrev');
-			}else {
-				$('.gonextpage').removeClass('active');
-				$('.gonextpage').data('dir','moveNext');
-			}
-		},
-		orientationchange: function () {}
-	});
-	$('.page').on('change',function(){
-
-	});
+});
 
 
-	$('.gonextpage').on('click', function (e) {
-		e.preventDefault();
-		$.fn.fullpage[$(this).data('dir')](true);
-	});
-
-})
-    
+$('.gonextpage').on('click', function (e) {
+	e.preventDefault();
+	$.fn.fullpage[$(this).data('dir')](true);
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,28 @@
 'use strict';
 
-$(function () {
+$('.wp-inner').fullpage({
+	page: '.page',
+	change: function change(e) {},
+	beforeChange: function beforeChange(e) {
+		setTimeout(function(){
+			$('.page').eq(e.cur).find('.animated').removeClass('active');
+		},800)
+	},
+	afterChange: function afterChange(e) {
+		$('.page').eq(e.cur).find('.animated').addClass('active');
+		if (Number(e.cur) === 3) {
+			$('.gonextpage').addClass('active');
+			$('.gonextpage').data('dir', 'movePrev');
+		} else {
+			$('.gonextpage').removeClass('active');
+			$('.gonextpage').data('dir', 'moveNext');
+		}
+	},
+	orientationchange: function orientationchange() {}
+});
+$('.page').on('change', function () {});
 
-	$('.wp-inner').fullpage({
-		page: '.page',
-		change: function change(e) {},
-		beforeChange: function beforeChange(e) {
-			$('.page').eq(e.cur).find('.items').removeClass('active');
-		},
-		afterChange: function afterChange(e) {
-			$('.page').eq(e.cur).find('.items').addClass('active');
-			if (Number(e.cur) === 3) {
-				$('.gonextpage').addClass('active');
-				$('.gonextpage').data('dir', 'movePrev');
-			} else {
-				$('.gonextpage').removeClass('active');
-				$('.gonextpage').data('dir', 'moveNext');
-			}
-		},
-		orientationchange: function orientationchange() {}
-	});
-	$('.page').on('change', function () {});
-
-	$('.gonextpage').on('click', function (e) {
-		e.preventDefault();
-		$.fn.fullpage[$(this).data('dir')](true);
-	});
+$('.gonextpage').on('click', function (e) {
+	e.preventDefault();
+	$.fn.fullpage[$(this).data('dir')](true);
 });
