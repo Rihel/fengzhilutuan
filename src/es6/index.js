260c114c9861a9e8b5d6ebebf10793623550d733
rihel <569843207@qq.com>
2017-03-10 00:04:36 +0800

    click btn transform page finish

--- src/es6/index.js
+++ src/es6/index.js
@@ -10,11 +10,25 @@ $(function(){
 		},
 		afterChange: function (e) {
 			$('.page').eq(e.cur).find('.items').addClass('active');
+			if(Number(e.cur)===3){
+				$('.gonextpage').addClass('active');
+				$('.gonextpage').data('dir','movePrev');
+			}else {
+				$('.gonextpage').removeClass('active');
+				$('.gonextpage').data('dir','moveNext');
+			}
 		},
 		orientationchange: function () {}
 	});
 	$('.page').on('change',function(){
 
-	})
+	});
+
+
+	$('.gonextpage').on('click', function (e) {
+		e.preventDefault();
+		$.fn.fullpage[$(this).data('dir')](true);
+	});
+
 })
     
--- src/js/index.js
+++ src/js/index.js
@@ -10,8 +10,20 @@ $(function () {
 		},
 		afterChange: function afterChange(e) {
 			$('.page').eq(e.cur).find('.items').addClass('active');
+			if (Number(e.cur) === 3) {
+				$('.gonextpage').addClass('active');
+				$('.gonextpage').data('dir', 'movePrev');
+			} else {
+				$('.gonextpage').removeClass('active');
+				$('.gonextpage').data('dir', 'moveNext');
+			}
 		},
 		orientationchange: function orientationchange() {}
 	});
 	$('.page').on('change', function () {});
+
+	$('.gonextpage').on('click', function (e) {
+		e.preventDefault();
+		$.fn.fullpage[$(this).data('dir')](true);
+	});
 });
